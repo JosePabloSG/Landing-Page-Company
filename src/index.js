46609fb195a7611456f9342976e8e49759fd9bb6
José Pablo Suárez Gómez <suarezgomezjosepablo03@gmail.com>
2023-11-05 18:48:12 -0600

    Update data.json with new content and rework index

--- src/index.js
+++ src/index.js
@@ -7,56 +7,79 @@ function render () {
   fetch('../src/mocks/data.json').then((response) => {
     return response.json()
   }).then((data) => {
-    const headerData = data.header
-    const heroData = data.hero
-    const servicesData = data.services
-    const galleryData = data.gallery
-    const eventsData = data.events
-    const footerData = data.footer
-    renderEvents(eventsData)
-    renderGallery(galleryData)
-    renderServices(servicesData)
-    renderHero(heroData)
-    renderHeader(headerData)
-    renderFooter(footerData)
+    renderData(data)
+    renderServices(data)
+    renderGallery(data)
+    renderEvents(data)
   }).catch((err) => {
     console.log(err)
   })
 }
 
-function renderHeader (headerData) {
-  const headerLogo = document.querySelector('.header-logo')
-  const headerTemplate = document.querySelector('.header-page-name')
+function renderData (data) {
+  const logoHeaderElement = document.querySelector('.header-logo')
+  const nameHeaderElement = document.querySelector('.header-page-name')
 
-  headerLogo.setAttribute('src', headerData.logo)
-  headerTemplate.innerHTML = headerData.name
+  const subtitleHeroElement = document.querySelector('.hero-subtitle')
+  const titleHeroElement = document.querySelector('.hero-title')
+  const paragraphHeroElement = document.querySelector('.hero-paragraph')
+  const imageHeroElement = document.querySelector('.img-hero')
+
+  const subtitleServicesElement = document.querySelector('.services-subtitle')
+  const titleServicesElement = document.querySelector('.services-title')
+  const paragraphServicesElement = document.querySelector('.services-paragraph')
+
+  const galleryTitleElement = document.querySelector('.gallery-title')
+
+  const eventsSubtitleElement = document.querySelector('.events-subtitle')
+  const eventsTitleElement = document.querySelector('.events-title')
+  const eventsParagraphElement = document.querySelector('.events-paragraph')
+
+  const footerImageElement = document.querySelector('.img-footer')
+  const footerTitleElement = document.querySelector('.footer-title')
+  const footerParagraphElement = document.querySelector('.footer-paragraph')
+  const footerHomeElement = document.querySelector('.home-home')
+  const footerHomeProductsElement = document.querySelector('.home-product')
+  const footerHomeAboutUsElement = document.querySelector('.home-about-us')
+  const footerContactElement = document.querySelector('.contact-contact')
+  const footerContactEmail = document.querySelectorAll('.contact-email')
+
+  logoHeaderElement.src = data.logoheader
+  nameHeaderElement.innerHTML = data.nameheader
+
+  subtitleHeroElement.innerHTML = data.subtitleHero
+  titleHeroElement.innerHTML = data.titleHero
+  paragraphHeroElement.innerHTML = data.paragraphHero
+  imageHeroElement.src = data.imageHero
+
+  subtitleServicesElement.innerHTML = data.subtitleServices
+  titleServicesElement.innerHTML = data.titleServices
+  paragraphServicesElement.innerHTML = data.paragraphServices
+
+  galleryTitleElement.innerHTML = data.galleryTitle
+
+  eventsSubtitleElement.innerHTML = data.subtitleEvents
+  eventsTitleElement.innerHTML = data.titleEvents
+  eventsParagraphElement.innerHTML = data.paragraphEvents
+
+  footerImageElement.src = data.imageFooter
+  footerTitleElement.innerHTML = data.titleFooter
+  footerParagraphElement.innerHTML = data.paragraphFooter
+  footerHomeElement.innerHTML = data.titleHome
+  footerHomeProductsElement.innerHTML = data.paragraphHomeProducts
+  footerHomeAboutUsElement.innerHTML = data.paragraphHomeAboutUs
+  footerContactElement.innerHTML = data.titleContact
+  footerContactEmail[0].innerHTML = data.paragraphContactAchaba
+  footerContactEmail[1].innerHTML = data.paragraphContactSuarez
 }
 
-function renderHero (heroData) {
-  const heroSubtitle = document.querySelector('.hero-subtitle')
-  const heroTitle = document.querySelector('.hero-title')
-  const heroParagraph = document.querySelector('.hero-paragraph')
-  const heroImage = document.querySelector('.img-hero')
 
-  heroSubtitle.innerHTML = heroData.subtitle
-  heroTitle.innerHTML = heroData.title
-  heroParagraph.innerHTML = heroData.paragraph
-  heroImage.setAttribute('src', heroData.image)
-}
-
-function renderServices (servicesData) {
-  const servicesSubtitle = document.querySelector('.services-subtitle')
-  const servicesTitle = document.querySelector('.services-title')
-  const servicesParagraph = document.querySelector('.services-paragraph')
-
-  servicesSubtitle.innerHTML = servicesData.subtitle
-  servicesTitle.innerHTML = servicesData.title
-  servicesParagraph.innerHTML = servicesData.paragraph
-
-  const servicesArray = servicesData.service
+function renderServices (data) {
+  console.log('Rendering services with data:', data.service)
+  const serviceArray = data.service
   const servicesContainer = document.querySelector('.services-cards')
 
-  servicesArray.forEach((service) => {
+  serviceArray.forEach((service) => {
     const card = document.createElement('div')
     card.classList.add('item-card-service')
 
@@ -76,76 +99,38 @@ function renderServices (servicesData) {
   })
 }
 
-function renderGallery (galleryData) {
-  const galleryTitle = document.querySelector('.gallery-title')
-  galleryTitle.innerHTML = galleryData.title
-
-  const galleryAray = galleryData.picture
+function renderGallery (data) {
+  const galleryArray = data.gallery
   const galleryContainer = document.querySelector('.gallery-images')
 
-  galleryAray.forEach((picture) => {
+  galleryArray.forEach((gallery) => {
     const card = document.createElement('div')
     card.classList.add('item-card-gallery')
 
     const image = document.createElement('img')
-    image.src = picture.image
+    image.src = gallery.image
     card.appendChild(image)
 
     galleryContainer.appendChild(card)
   })
 }
 
-function renderEvents (eventsData) {
-  const eventsSubtitle = document.querySelector('.events-subtitle')
-  const eventsTitle = document.querySelector('.events-title')
-  const eventsParagraph = document.querySelector('.events-paragraph')
-
-  eventsSubtitle.innerHTML = eventsData.subtitle
-  eventsTitle.innerHTML = eventsData.title
-  eventsParagraph.innerHTML = eventsData.paragraph
-
-  const eventsArray = eventsData.event
+function renderEvents (data) {
+  const eventsArray = data.events
   const eventsContainer = document.querySelector('.events-cards')
 
-  eventsArray.forEach((event) => {
+  eventsArray.forEach((events) => {
     const card = document.createElement('div')
     card.classList.add('item-card-event')
 
     const image = document.createElement('img')
-    image.src = event.image
+    image.src = events.image
     card.appendChild(image)
 
     const title = document.createElement('h3')
-    title.innerHTML = event.title
+    title.innerHTML = events.title
     card.appendChild(title)
+
     eventsContainer.appendChild(card)
-  })
-}
-
-function renderFooter (footerData) {
-  // Hace conocer el HTML al javaScript nombrando las variables que traen las etiquetas HTML con el querrySelector
-  const footerLogo = document.querySelector('.img-footer')
-  const footerTitle = document.querySelector('.footer-title')
-  const footerParagraph = document.querySelector('.footer-paragraph')
-  const footerHome = document.querySelector('.home-home')
-  const footerHomeProducts = document.querySelector('.home-product')
-  const footerHomeAboutUs = document.querySelector('.home-about-us')
-  const footerContact = document.querySelector('.contact-contact')
-  const footerContactEmails = document.querySelectorAll('.contact-email') // Se usa para asignarle a todos los que tengan el mismo nombre los elementos del json
-
-  // Asigna los valores del json a las variables creadas
-  footerLogo.setAttribute('src', footerData.image)
-  footerTitle.innerHTML = footerData.title
-  footerParagraph.innerHTML = footerData.paragraph
-  // Accede al arreglo 'home' y obtén el elemento específico
-  footerHome.innerHTML = footerData.home[0].title // Accedes al primer elemento
-  // Accede al arreglo 'home' y obtén otros elementos si es necesario
-  footerHomeProducts.innerHTML = footerData.home[1].paragraph // Accedes al segundo elemento
-  footerHomeAboutUs.innerHTML = footerData.home[2].paragraph // Accedes al tercer elemento
-  // Accede al arreglo 'contact' y obtén elementos específicos
-  footerContact.innerHTML = footerData.contact[0].title // Accedes al primer elemento del arreglo 'contact'
-  // Utiliza forEach para asignar los valores de los correos electrónicos en el arreglo 'contact'
-  footerContactEmails.forEach((element, index) => {
-    element.innerHTML = footerData.contact[index + 1].paragraph
   })
 }
