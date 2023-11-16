b1392017de941506e0d10b0dbaab66d235bed939
Brandon Carrillo Álvarez <128259306+BrandonCA859@users.noreply.github.com>
2023-11-16 03:08:35 -0600

    El update de los servicios ya con su fetch

--- Page/admin.js
+++ Page/admin.js
@@ -2,11 +2,10 @@ window.onload = () => {
   console.log('La página se ha cargado completamente');
   render();
 
-  // Agregamos un evento de clic al botón de eliminación
+
   const deleteGalleryButton = document.getElementById('Btn-Delete');
   if (deleteGalleryButton) {
     deleteGalleryButton.addEventListener('click', () => {
-      // Reemplaza 'galleryIdToDelete' con el ID real de la galería que deseas eliminar
       const galleryIdToDelete = document.querySelector('#input-delete').value;
       deleteGallery(galleryIdToDelete);
     });
@@ -14,7 +13,7 @@ window.onload = () => {
 };
 
 function render() {
-  // Utilizamos la dirección correcta para obtener datos de galerías
+  
   fetch('http://localhost:3000/galleries/').then((response) => {
     return response.json();
   }).then((data) => {
@@ -23,19 +22,18 @@ function render() {
     renderGallery(data);
     renderEvents(data);
   }).catch((err) => {
-    // console.log(err); // Comentar o eliminar esta línea si no quieres ver mensajes de error en la consola
+  
   });
 }
 
 function deleteGallery(galleryId) {
-  // Utilizamos la dirección correcta para eliminar la galería
+
   fetch(`http://localhost:3000/galleries/${galleryId}`, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      // Puedes añadir más datos aquí según los requisitos de tu API
     }),
   })
     .then((response) => {
@@ -47,12 +45,41 @@ function deleteGallery(galleryId) {
     .then((data) => {
       console.log('Galería eliminada exitosamente:', data);
       renderGallery(data);
-      // console.log('¡La galería se eliminó exitosamente!'); // Comentar o eliminar esta línea si no quieres ver mensajes de éxito en la consola
     })
     .catch((error) => {
-      // console.error('Error al eliminar la galería:', error); // Comentar o eliminar esta línea si no quieres ver mensajes de error en la consola
-      // displayErrorMessage('Error al eliminar la galería. Por favor, inténtalo de nuevo.'); // Comentar o eliminar esta línea si no quieres mostrar mensajes de error en la consola
+    
     });
 }
 
+const updateButton = document.querySelector('#Btn-Update');
+if (updateButton) {
+  updateButton.addEventListener('click', () => {
+    const serviceIdToUpdate = document.querySelector('#input-update').value;
+    const updatedData = {
+      title: document.querySelector('.btn-title').value,
+      paragraph: document.querySelector('.btn-Description').value,
+      url: document.querySelector('.btn-image').value,
+    };
 
+    fetch(`http://localhost:3000/services/${serviceIdToUpdate}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedData),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('La respuesta de la red no fue exitosa');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log('El servicio fue actualizado exitosamente:', data);
+        renderServices(data);
+      })
+      .catch((error) => {
+        console.error('Error al actualizar el servicio:', error);
+      });
+  });
+}
